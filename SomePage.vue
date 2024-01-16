d47468a75c1dac17981b76da961b2c3b358bba56
T-H9703EnAc <105440150+T-H9703EnAc@users.noreply.github.com>
2024-01-17 01:38:06 +0900

    Create SomePage.vue

---
+++ SomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>方角を選択してください</h1>
+    <CustomRadioButton v-model="selectedDirection" />
+    <!-- 他のコンテンツやロジック -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CustomRadioButton from './CustomRadioButton.vue';
+
+const selectedDirection = ref('');
+// 他のスクリプトやメソッド
+</script>
+
+<style>
+/* スタイル */
+</style>
