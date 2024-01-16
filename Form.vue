47a689c43d1dc81a9db874448e8257ce557aefa8
T-H9703EnAc <105440150+T-H9703EnAc@users.noreply.github.com>
2024-01-17 01:22:36 +0900

    Create Form.vue

---
+++ Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <CustomTextbox v-model="textValue" />
+  <CustomRadioButton :options="radioOptions" v-model="radioValue" />
+  <button @click="handleSubmit">送信</button>
+</template>
+
+<script setup>
+import CustomTextbox from './CustomTextbox.vue';
+import CustomRadioButton from './CustomRadioButton.vue';
+import { ref } from 'vue';
+
+const textValue = ref('');
+const radioOptions = [{ value: 'option1' }, { value: 'option2' }];
+const radioValue = ref('');
+
+const handleSubmit = () => {
+  // ここで textValue と radioValue に基づいて何かの処理を行う
+  // 例: console.log(textValue.value, radioValue.value);
+};
+</script>
