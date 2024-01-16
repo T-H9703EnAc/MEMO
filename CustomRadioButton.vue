55356df44c0cb040a904ade4501c753884026320
T-H9703EnAc <105440150+T-H9703EnAc@users.noreply.github.com>
2024-01-17 01:23:27 +0900

    Create CustomRadioButton.vue

---
+++ CustomRadioButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      :value="option.value"
+      v-for="option in options"
+      :key="option.value"
+      :checked="option.value === value"
+      @change="$emit('update:value', option.value)"
+    >
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  value: String,
+  options: Array
+});
+</script>
