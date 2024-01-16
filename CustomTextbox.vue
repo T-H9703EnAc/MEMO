c5d8c9e30e58693ec7fd010e01427079395facb1
T-H9703EnAc <105440150+T-H9703EnAc@users.noreply.github.com>
2024-01-17 01:23:53 +0900

    Create CustomTextbox.vue

---
+++ CustomTextbox.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    type="text"
+    :value="value"
+    @input="$emit('update:value', $event.target.value)"
+  >
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  value: String
+});
+</script>
